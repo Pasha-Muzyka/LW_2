0f1dc85747b3c0c938d48cdde6f2ba68c5b9369d
Pasha-Muzyka <xa12850450@student.karazin.ua>
2020-11-27 11:03:20 +0200

    version 0.1

---
+++ addUser.php
@@ -0,0 +1,72 @@
+<?php
+	session_start();
+	require_once 'connection.php';
+		
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+	$password_confirm = $_POST['password_confirm'];
+	if($_POST['role'] == "admin") {
+		$id_role = 1;
+	} else {
+		$id_role = 2;
+	}
+
+	if($password === $password_confirm){
+	
+		$target_dir = "public/images/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+				$uploadOk = 1;
+			} else {
+				$_SESSION['message'] = 'File is not an image.';
+				$uploadOk = 0;
+			}
+		}
+		// Check if file already exists
+		if (file_exists($target_file)) {
+			$_SESSION['message'] = $_SESSION['message'] . '<br>' . 'Sorry, file already exists.';
+			$uploadOk = 0;
+		}
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+			$_SESSION['message'] = $_SESSION['message'] . '<br>' . 'Sorry, your file is too large.';
+			$uploadOk = 0;
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+		&& $imageFileType != "gif" ) {
+			$_SESSION['message'] = $_SESSION['message'] . '<br>' . 'Sorry, only JPG, JPEG, PNG & GIF files are allowed.';
+			$uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			$_SESSION['message'] = $_SESSION['message'] . '<br>' . 'Sorry, your file was not uploaded.';
+			header('Location: signUp.php');
+		// if everything is ok, try to upload file
+		} else {
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+			} else {
+				echo "";
+				$_SESSION['message'] = $_SESSION['message'] . '<br>' . 'Sorry, there was an error uploading your file.';
+				header('Location: signUp.php');
+			}
+		}		
+
+		mysqli_query($conn, "INSERT INTO `users` (`first_name`, `last_name`, `password`, `photo`, `role_id`) 
+		VALUES ('$first_name', '$last_name', '$password', '$target_file', '$id_role')"); 
+
+		mysqli_close($conn);
+		header('Location: login.php');
+	}else {
+        $_SESSION['message'] = 'Пароли не совпадают';
+        header('Location: signUp.php');
+    }
+?>
---
+++ signUp.php
@@ -0,0 +1,32 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <link rel="stylesheet" href="css/layout.css">
+</head>
+<body>
+
+	<form action="addUser.php" method="post" enctype="multipart/form-data">
+
+		<label>Имя</label><br>
+    	<input type="text" name="first_name" placeholder="Введите свое имя"><br>
+    	<label>Фамилия</label><br>
+		<input type="text" name="last_name" placeholder="Введите свою фамилию"><br>
+		<label>Логин</label><br>
+        <input type="text" name="login" placeholder="Введите свой логин"><br>
+        <label>Пароль</label><br>
+        <input type="password" name="password" placeholder="Введите пароль"><br><br>
+        <label>Подтверждение пароля</label><br>
+        <input type="password" name="confirm_password" placeholder="Подтвердите пароль"><br><br>
+		<select id="role" name='role'>
+                <option disabled="disabled" selected="selected">Роль</option>
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+        </select><br><br>
+
+    	<button type="submit">Зарегистрироваться</button>
+	</form>
+
+</body>
+</html>
